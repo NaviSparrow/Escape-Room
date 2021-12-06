aff293fa717b530256bfb3ec20db74015d5ed878
Ivan <work.gorobec@gmail.com>
2021-12-06 22:56:14 +0500

    создаст компонент для констант

---
+++ src/services/const.js
@@ -0,0 +1,13 @@
+export const APIRoute = {
+  QuestList: 'quests',
+  NewOrder: 'orders',
+};
+
+export const QuestType = {
+  All: 'Все квесты',
+  Adventures: 'Приключения',
+  Horrors: 'Ужасы',
+  Mystic: 'Мистика',
+  Detective: 'Детектив',
+  SciFi: 'Sci-fi',
+};
