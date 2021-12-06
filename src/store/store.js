5e215f45e3e869dc373e8be207475783c5288110
Ivan <work.gorobec@gmail.com>
2021-12-06 22:55:31 +0500

    создаст компонент для объявления store и селекторов

---
+++ src/store/store.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, createStore } from '@reduxjs/toolkit';
+import { reducer } from './reducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+export const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(
+    thunk)),
+);
+
+export const selectQuestType = state => state.questType;
+export const selectQuestList = state => state.questList;
+export const selectDetailedQuest = state => state.detailedQuest;
