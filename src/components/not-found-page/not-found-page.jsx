0ba6067810a76b93b556632a9db585415ee7342b
Ivan <work.gorobec@gmail.com>
2021-12-09 23:32:39 +0500

    рефакторинг компонента страницы 404

--- src/components/not-found-page/not-found-page.jsx
+++ src/components/not-found-page/not-found-page.jsx
@@ -2,30 +2,30 @@ import * as S from '../common/header/header.styled';
 import * as C from '../detailed-quest/detailed-quest.styled';
 
 import logo from '../../assets/img/logo.svg';
+import { useHistory } from 'react-router-dom';
+import { AppRoute } from '../../services/const';
 
-const NotFoundPage = () => (
-  <>
-    <S.StyledHeader>
-      <S.HeaderWrapper>
-        <S.Logo>
-          <S.Image src={logo} alt="Логотип Escape Room" width="134" height="50" />
-        </S.Logo>
+const NotFoundPage = () => {
+  const history = useHistory();
+  return (
+    <>
+      <S.StyledHeader>
+        <S.HeaderWrapper>
+          <S.Logo>
+            <S.Image src={logo} alt="Логотип Escape Room" width="290" height="150" />
+          </S.Logo>
+        </S.HeaderWrapper>
+      </S.StyledHeader>
 
-        <S.LinkItem>
-          <S.Link $isActiveLink to="/">
-            Вернуться на главную
-          </S.Link>
-        </S.LinkItem>
-
-      </S.HeaderWrapper>
-    </S.StyledHeader>
-
-    <C.Main>
-      <C.PageHeading>
-        <C.PageTitle>404. Page not found</C.PageTitle>
-      </C.PageHeading>
-    </C.Main></>
-
-);
+      <C.Main>
+        <C.PageHeading>
+          <C.PageTitle>404. Page not found</C.PageTitle>
+        </C.PageHeading>
+        <C.QuestBookingBtn onClick={() => history.push(AppRoute.Root)}>
+          Вернуться на главную
+        </C.QuestBookingBtn>
+      </C.Main></>
+  );
+}
 
 export default NotFoundPage;
